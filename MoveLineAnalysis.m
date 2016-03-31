77dffe23c92b23e14dd53b74caa73c81a081d48c
abigailrilee <abigailrilee@googlemail.com>
2016-03-31 15:34:05 +0100

    Changes so multiple blocks of same paradigm type can be analysed at once
    I've made it so there are 10 reps of each condition in each paradigm,
    which means we'll have three repeats of each paradigm interleaved. So
    that we can still analyse all of the data from each separate block of
    the same paradigm, I've altered the basic analysis file so that it will
    take all of the data files of a certain condition and subject (currently
    still have to enter manually but want to automate) and combine them so
    they can be analysed together.
    
    Also added some comments.

--- MoveLineAnalysis.m
+++ MoveLineAnalysis.m
@@ -1,15 +1,35 @@
 cd /Users/Abigail/Documents/psychtoolboxProjects/psychMaster/Data
 %cd C:\Users\aril\Documents\Data
-%Need to automate loading and loop it so that multiple files can be
-%analysed at once.
-load('MoveLine_combined_away_ALp_20160222_154924');
 
-ResponseTable = struct2table(experimentData); %The data struct is converted to a table
+%Eventually want it so that you can specify the initials and the conditions
+%to load from within the piece of code.
+
+% condition = {'combined_towards'};
+% subjectInitials = {'ALp'};
+% fileNameString = strcat('MoveLine_', condition, '_', subjectInitials,'_');
+% fileNameStart = char(fileNameString);
+
+%filenames = dir('C:\Users\aril\Documents\Data\MoveLine_combined_towards_ALp*'); %for the lilac room
+filenames = dir('/Users/Abigail/Documents/psychtoolboxProjects/psychMaster/Data/MoveLine_combined_towards_ALp*'); %for the lab mac
+%will load all AL pilots in the combined towards paradigm
+filenames = {filenames.name}; %makes a cell of filenames
+i = 1;
+for i = 1:length(filenames)
+    filenamestr = char(filenames(i));
+    dataFile(i) = load(filenamestr); 
+    i = i+1;
+    %creates a struct with the data from each loaded file, so that data 
+    %from different blocks of the same condition can be loaded to be analysed together.
+end
+
+allExperimentData = [dataFile.experimentData]; %all of the experiment data in one combined struct
+allSessionInfo = dataFile.sessionInfo; %all of the session info data in one combined struct
+ResponseTable = struct2table(allExperimentData); %The data struct is converted to a table
 
 %excluding invalid trials
 wantedData = ~(ResponseTable.validTrial == 0); %creates a logical of which trials in the data table were valid
-validIsResponseCorrect = ResponseTable.isResponseCorrect(wantedData); %
-validCondNumber = ResponseTable.condNumber(wantedData);
+validIsResponseCorrect = ResponseTable.isResponseCorrect(wantedData); %finding the correct responses excluding invalid responses
+validCondNumber = ResponseTable.condNumber(wantedData); %finding the condition number of the valid responses
 if iscell(validIsResponseCorrect) %if this is a cell because there were invalid responses
     correctResponsesArray = cell2mat(validIsResponseCorrect); %convert to an array 
     correctResponsesLogical = logical(correctResponsesArray); %then convert to a logical
@@ -30,15 +50,15 @@ allTrialNumbers = histc(validCondNumber, allTrialConditions); %the total number 
 
 allCorrectPercentages = (condCorrectNumbers./allTrialNumbers)*100; %creates a double of the percentage correct responses for every condition
 
-if length(allTrialNumbers) > 7
+if length(allTrialNumbers) > 7 %if there's more than 7 conditions because there's both lateral and depth presentations
 allDepthPercentageCorrect = allCorrectPercentages(1:7);
 allLateralPercentageCorrect = allCorrectPercentages(8:14);
 else
     allDepthPercentageCorrect = allCorrectPercentages;
 end
 
-conditionFirstSectionVelocities = [sessionInfo.conditionInfo.velocityCmPerSecSection1];
-FirstVelocities = unique(conditionFirstSectionVelocities);
+conditionFirstSectionVelocities = [allSessionInfo.conditionInfo.velocityCmPerSecSection1]; %looking at the initial speed of the movement of the line to plot on the graph
+FirstVelocities = unique(conditionFirstSectionVelocities); %we don't want the entire matrix of hundreds of the same speeds over and over again, just want to know the speeds, so use unique.
 %get rid of negative sign as it doesn't matter which direction the condition was here.
 if min(FirstVelocities) < 0;
     normalisedFirstVelocities = FirstVelocities*-1;
@@ -49,7 +69,8 @@ else
 end
 
 
-%Drawing the graph of percentage "the condition was faster" responses
+%Drawing the graph of percentage "the condition was faster" responses for
+%depth conditions
 figure
 plot(orderedVelocities, allDepthPercentageCorrect, '-xk');
 axis([min(orderedVelocities) max(orderedVelocities) 0 100]);
@@ -58,7 +79,7 @@ xlabel('Velocity of the first section (cm/s)');
 ylabel('Percentage correct responses');
 title('towards');
 
-if length(allTrialNumbers) > 7
+if length(allTrialNumbers) > 7 %if there were lateral conditions, drawing a second graph for those conditions
 figure
 plot(orderedVelocities, allLateralPercentageCorrect, '-xk');
 axis([min(orderedVelocities) max(orderedVelocities) 0 100]);
