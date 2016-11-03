1f99b7cc3ccf1719fdb9bac384775695163d7531
fa28 <fa28@st-andrews.ac.uk>
2016-11-03 13:46:28 +0000

    diff bonds

--- whit_rep.m
+++ whit_rep.m
@@ -4,13 +4,13 @@ fileToLoad = uigetfile; load(fileToLoad);
 [sortedData] = organizeData(sessionInfo,experimentData);
 
 
-iCond = 1; %When you have only 1 condition
+iCond = 2; %When you have only 1 condition
 respOri = [sortedData(iCond).trialData(:).respOri];
 stimOri = [sortedData(iCond).trialData(:).stimOri];
 
 RO_initial_time_point=0;
 
-    for i=  2:length(respOri);
+    for i=  1:length(respOri);
         %respOri(i)=stimOri(i);
         
         %err(i) = minAngleDiff(respOri, stimOri);
