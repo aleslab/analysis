9dc00d120ff624991bdbb977348a8a5f4893c57b
fa28 <fa28@st-andrews.ac.uk>
2016-11-08 16:15:03 +0000

    put in gain calc

--- whit_kal.m
+++ whit_kal.m
@@ -5,8 +5,8 @@
 % 
 % 
 % iCond =2; %When you have only 1 condition
-% Z = [sortedData(iCond).trialData(:).respOri];
-% B = [sortedData(iCond).trialData(:).stimOri];
+Z = [sortedData(iCond).trialData(:).respOri];
+B = [sortedData(iCond).trialData(:).stimOri];
 
 
 %27014471509;-57.3335435238649;-51.1634861330904;-150.364066752811;-47.8948887608929;27.1291776387804;137.057547347594;-140.530117657153;-36.5066023797064];
@@ -15,10 +15,10 @@
 
 Xinitial_time_point=0;
 Pinitial=9;
-R = 1;
+R = var(B);
 Xhat = [];
 Xhat(1)=Xinitial_time_point;
-P(1)=Pinitial;
+P=var(Z);
 K(1)=P(1)/(P(1)+R);
 Xhat (1)= Xhat(1)+ K(1)*(Z(1)-Xhat(1));
 P(1)=(1-K(1))*P(1);
