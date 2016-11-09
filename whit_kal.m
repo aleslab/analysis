997c07c480930a5819d68796133d750bbbb454b0
fa28 <fa28@st-andrews.ac.uk>
2016-11-09 14:00:59 +0000

    runs but doesn't plot

--- whit_kal.m
+++ whit_kal.m
@@ -2,6 +2,13 @@
 
 value = [0.39	0.50	0.48	0.29	0.25	0.32	0.34	0.48	0.41	0.45]; % value = est
 
+% fileToLoad = uigetfile; load(fileToLoad);
+% [sortedData] = organizeData(sessionInfo,experimentData);
+% 
+% 
+% iCond =2; %When you have only 1 condition
+% respOri = [sortedData(iCond).trialData(:).respOri];
+% stimOri = [sortedData(iCond).trialData(:).stimOri];
 
 estimate_initial_time_point = 0;%define value for first Xhat
 
@@ -9,39 +16,44 @@ estimate(1) = estimate_initial_time_point; % tell matlab that the first Xhat
 distal_initial_time_point = 1;
 distal = 100;
 proximal = 120;
-gain = distal_inital_time_point / (distal_initial_time_point+proximal);
-estimate = estimate(1)+gain*value(1)-esimate(i);
-prediction_error = value - new_estimate;
 
-for i= 2:length (value);
+gain = distal / (distal +proximal);
+estimate = estimate+gain*value - estimate;
+
+
+for i= 1:length (value);
     estimate(i)=estimate(i-1);
-    gain(i) =  distal_initial_time_point(i)/ (distal(i)+proximal(i));
-    estimate(i)=estimate(i) + gain(i)*(prediction_error(i));
+    gain(i) =  (i-1)/ (distal(i)+proximal(i-1));
+    estimate(i)=estimate(i-1) + gain(i)*(value(i)-estimate(i));
     gain(i)=(1-gain(i)*(distal(i)));
     
 end
 
-plot (value, 'm');
+plot (value);
 hold on
-plot (estimate, 'c');
-
-
-
-
-
-
-
-
-
-
-
-plot(Z,'m')
-hold on;
-plot(Xhat,'c')
-plot(B, 'k')
-legend('Stim movement', 'how the kalman tracks stim', 'how partcipant tracks ') 
+% plot (gain, 'r');
+% hold on
+plot (estimate);
+hold off
 
 grid on
+
+
+
+
+
+
+
+
+
+
+% plot(Z,'m')
+% hold on;
+% plot(Xhat,'c')
+% plot(B, 'k')
+% legend('Stim movement', 'how the kalman tracks stim', 'how partcipant tracks ') 
+% 
+% grid on
  
 
 
