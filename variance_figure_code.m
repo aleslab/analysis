3b462e7ed83704222831c64e6af1945dd8df83e3
fa28 <fa28@st-andrews.ac.uk>
2016-11-17 16:45:44 +0000

    changes

---
+++ variance_figure_code.m
@@ -0,0 +1,7 @@
+v= []'% put variance figures in one condition at a time i.e all con1's then all con2's
+
+bar1=bar(v, 'grouped');
+ylabel('partcipant number'); % y-axis label
+ylabel('proximal variance (var in error'); % y-axis label
+
+%calculate means for all cond
