545d1ddfa3298289758c2e1592c6a981e03f0f2a
fa28 <fa28@st-andrews.ac.uk>
2016-11-29 15:26:00 +0000

    trying a few things
    correlation in kalman estimate and RO

--- whit_rep.m
+++ whit_rep.m
@@ -28,11 +28,7 @@ std(err)
 var_prox=var(err); % amount of variance in the error
 var_dist=var(stimOri); %variance in the stimOri
 gain=var_dist / (var_dist + var_prox);
-%resp_time=(sortedData(3).trialData.responseTime); % mean response times for each con
-%MT=mean(resp_time);% NEED TO CHANGE THE NUMBER OF SORTED DATA EACH TIME YOU RUN
-% FOR EXAMPLE sortedData(?) to match the condition number
 
-%cor=(corrcoef(respOri(i),stimOri(i-1));
 
 [R,P]=corrcoef(err,RO);
 
@@ -44,18 +40,7 @@ xlabel('relative orientation of previous trial'); % x-axis label
 ylabel('error on current trial (deg)'); % y-axis label
 
 
-% figure
-%        plot (var_dist, 'r');
-%        hold on
-%        plot (var_prox, 'g');
-%        hold on 
-%        plot (gain, 'b');
-%        
 
-
-% filename={'P1_whit_com.mat'};
-% save('P1_whit_com.mat');
-% 
 
     
 
