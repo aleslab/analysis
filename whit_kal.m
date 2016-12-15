f81531d45ccceb077ec36d1dd9950b9cdd22165f
fa28 <fa28@st-andrews.ac.uk>
2016-12-15 10:49:55 +0000

    changes

--- whit_kal.m
+++ whit_kal.m
@@ -3,13 +3,12 @@
 clear all;
 
 
-stimOri = wrapTo90(360*rand(40,1));
+stimOri = wrapTo90(360*rand(200,1));
 var_prox = 100;
 
-
 %This line is to simulate proximal noise from the observer. 
-%value=stimOri+randn(size(stimOri))*sqrt(var_prox);
-value = stimOri;
+value=stimOri+randn(size(stimOri))*sqrt(var_prox);
+%value = stimOri;
 %stimOri = value;
 
 clear estimate;
@@ -34,7 +33,7 @@ distal_initial_time_point = 1;
 % var_dist=var(stimOri); %variance in the stimOri
 % gain=var_dist / (var_dist + var_prox);
 
-gain = .1;%var_dist / (var_dist + var_prox);
+gain = .95;%var_dist / (var_dist + var_prox);
 %estimate = estimate+gain*value - estimate;
 
 err(1) = 0;
@@ -47,56 +46,64 @@ for i= 2:length (value);
 
     
     estimate(i)=estimate(i-1) + gain*minAngleDiff(value(i),estimate(i-1));
-    sdEstimate(i)  = value(i-1) + gain*minAngleDiff(value(i),value(i-1));
-    estimate(i) = wrapTo90(estimate(i));
     
+%    sdEstimate(i) = gain*value(i) + (1-gain)*value(i-1);
+    sdEstimate(i)  = value(i-1) + gain*minAngleDiff(value(i),value(i-1));
+
+    estimate(i) = wrapTo90(estimate(i));
+    %gain(i)=(1-gain*(distal(i)));
+    
+    % err(i) = minAngleDiff(estimate(i), stimOri(i));
+     
+     %RO(i)=  minAngleDiff (stimOri(i-1),stimOri(i));
+     
      err(i) = minAngleDiff(estimate (i), stimOri(i));%whitney
      RO(i)=  minAngleDiff (stimOri(i-1),stimOri (i));%whitney
      PE(i) = minAngleDiff(stimOri(i),estimate (i-1));%PE
-     estimateUpdate(i) =  minAngleDiff(estimate(i),estimate (i-1)); %how much the est updates
+     estimateUpdate(i) =  minAngleDiff(estimate(i),estimate (i-1));
      
      sdErr(i) = minAngleDiff(sdEstimate (i), stimOri(i));%whitney
      
     
 end
 
-[R,P]=corrcoef(PE,RO);
-average= mean(PE);
+[R,P]=corrcoef(err,RO);
 
 
-% figure(101);
-% clf;
-% plot (p_response);
-% 
-% hold on
-% plot (stimOri, 'x');
-% plot (estimate, 'c*');
-% legend ('p_response', 'actual_stimOri', 'modelled kalman resposne given calibrated gain')
-% xlabel('trial number')
-% ylabel ('orientation');
-% 
-% 
-% figure(102);clf;
-% scatter (RO, err,'r','filled');
-% legend ('error on trial')
-% xlabel('RO');
-% ylabel('error');
+
+figure(101);
+clf;
+%plot (p_response);
+
+hold on
+plot (stimOri, 'x');
+plot (estimate, 'c*');
+legend ('p_response', 'actual_stimOri', 'modelled kalman resposne given calibrated gain')
+xlabel('trial number')
+ylabel ('orientation');
+
+
+figure(102);clf;
+scatter (RO, err,'r','filled');
+legend ('error on trial')
+xlabel('RO');
+ylabel('error');
 % hold on
 % lsline(ax1);
-% 
-% figure (103);clf;
-% scatter (RO, PE,'b','filled');
-% legend ('prediction error');
-% xlabel ('RO');
-% ylabel('amount of prediciton error');
-% 
-% 
-% figure (104);clf;
-% scatter (estimateUpdate, PE,'b','filled');
-% legend ('responseUpdate');
-% xlabel('how much the estimate updates');
-% ylabel ('amount of prediction error');
-% 
+
+figure (103);clf;
+scatter (RO, PE,'b','filled');
+legend ('prediction error');
+xlabel ('RO');
+ylabel('amount of prediciton error');
+
+
+figure (104);clf;
+scatter (estimateUpdate, PE,'b','filled');
+legend ('responseUpdate');
+xlabel('how much the estimate updates');
+ylabel ('amount of prediction error');
+
 
 
 figure(105);clf;
