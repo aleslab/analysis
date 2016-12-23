d1165e71bd36f5839b56456b4be39061e4420d9d
fa28 <fa28@st-andrews.ac.uk>
2016-12-23 22:18:07 +0000

    reduce trial numbers for illustration figure

--- whit_kal.m
+++ whit_kal.m
@@ -3,7 +3,7 @@
 clear all;
 
 
-stimOri = wrapTo90(360*rand(200,1));
+stimOri = wrapTo90(360*rand(20,1));
 var_prox = 100;
 
 %This line is to simulate proximal noise from the observer. 
@@ -27,6 +27,7 @@ estimate_initial_time_point = 0;%define value for first Xhat
 
 estimate(1) = estimate_initial_time_point; % tell matlab that the first Xhat 
 distal_initial_time_point = 1;
+gain = .9;%distal / (distal +proximal);
 
 err(1) = 0;
 RO(1)  = 0;
@@ -61,39 +62,45 @@ end
 
 figure(101);
 clf;
+set (gca,'fontsize', 22);
 %plot (p_response);
 
 hold on
-plot (stimOri, 'x');
-plot (estimate, 'c*');
-legend ('p_response', 'actual_stimOri', 'modelled kalman resposne given calibrated gain')
-xlabel('trial number')
-ylabel ('orientation');
+plot (stimOri,'g', 'Linewidth',2);
+hold on
+plot (estimate,'k','Linewidth',2);
+legend ('Kalman prediction of blades given gain of 0.5', 'Actual blade orientaton');
+xlabel('Time recorded');
+ylabel ('Orientation of turbine blades in degrees');
 
-
-figure(102);clf;
-scatter (RO, err,'r','filled');
-legend ('error on trial')
-xlabel('RO');
-ylabel('error');
-
-figure (103);clf;
-scatter (RO, PE,'b','filled');
-legend ('prediction error');
-xlabel ('RO');
-ylabel('amount of prediciton error');
-
-
-figure (104);clf;
+% 
+% figure(102);clf;
+% scatter (RO, err,'r','filled');
+% legend ('error on trial')
+% xlabel('RO');
+% ylabel('error');
+% 
+% figure (103);clf;
+% set (gca,'fontsize', 22);
+% scatter (RO, PE,'b','filled');
+% legend ('prediction error');
+% xlabel ('RO');
+% ylabel('amount of prediciton error');
+% 
+% 
+figure (104);
+clf;
+set (gca,'fontsize', 22);
+hold on
 scatter (estimateUpdate, PE,'b','filled');
 legend ('responseUpdate');
 xlabel('how much the estimate updates');
 ylabel ('amount of prediction error');
-
-
-
-figure(105);clf;
-scatter (RO, sdErr,'g','filled');
-legend ('whitney_sdErr')
-
-
+% 
+% 
+% 
+% figure(105);clf;
+% scatter (RO, sdErr,'g','filled');
+% legend ('whitney_sdErr')
+% 
+% 
