bcb1a896c4876c25a61d1afb3a0defc21fb0001d
fa28 <fa28@st-andrews.ac.uk>
2017-01-11 13:49:04 +0000

    changes for psycholoquia figures in simulation code
    changes to font size, legend etc

--- neat_kal.m
+++ neat_kal.m
@@ -7,9 +7,10 @@
 %iCond =2; %When you have only 1 condition
 % B = [sortedData(iCond).trialData(:).respOri];
 % Z = [sortedData(iCond).trialData(:).stimOri];
+Z=(20*rand(30,1));
 
-Z=360*rand(20,1);
-B=Z+rand(20,1);
+%Z=360*rand(20,1);
+%B=wrapTo90(Z+rand(20,1));
 Xinitial_time_point=0;
 Pinitial=1;
 R = 1;
@@ -17,18 +18,20 @@ Xhat = [];
 Xhat(1)=Xinitial_time_point;
 P(1)=Pinitial;
 K(1)=P(1)/(P(1)+R);
-Xhat (1)= Xhat(1)+ K(1)*(Z(1)-Xhat(1));
+Xhat (1)= Xhat(1)+ K(1)*minAngleDiff(Z(1),Xhat(1));
 P(1)=(1-K(1))*P(1);
 for k=2:length(Z), 
     Xhat(k)=Xhat(k-1);
     %P(k)=P(k-1);
-    K(k)=0.43;%gain
+    K(k)=0.9;%gain
     Xhat(k)=Xhat(k)+ K(k)*(Z(k)-Xhat(k));
     %P(k)=P(k);%(1-K(k))*P(k);
     
+    RO(k)=  minAngleDiff(Z(k-1),Z (k));
+    err(k) =minAngleDiff(Xhat(k)- Z(k));%whitney
     
-   estimateUpdate(k) =  (Xhat(k)-Xhat (k-1));
-   PE(k) = (Z(k)- Xhat(k-1));%PE
+   estimateUpdate(k) = minAngleDiff(Xhat(k),Xhat (k-1));
+   PE(k) = minAngleDiff(Z(k), Xhat(k-1));%PE
 end
 figure (101)
 clf
@@ -37,10 +40,10 @@ hold on
 plot(Z,'r','Linewidth', 3);
 hold on;
 plot(Xhat,'k', 'Linewidth', 3);
-legend('Actual blade orientation','Kalman prediction');
+legend('Actual position','Kalman/GPS prediction');
 
 xlabel('Time recorded');
-ylabel ('Orientation of blades in degrees');
+ylabel ('Movement  of car on road');
 
 figure (102);
 clf;
@@ -50,9 +53,16 @@ hold on
 scatter (estimateUpdate, PE, 200,'k','filled'); 
 %set(estimateUpdate,'SizeData',20);
 hold on
-legend ('Prediction update in degrees');
-xlabel('Prediction updates in degrees');
-ylabel ('Prediction error in degrees');
+legend ('Prediction update in meters ');
+xlabel('Prediction updates in meters');
+ylabel ('Prediction error in meters');
+
+figure (103);
+clf;
+scatter (RO, err, 200,'k','filled'); 
+legend ('error on trial');
+xlabel('RO');
+ylabel('error');
 
 
     
