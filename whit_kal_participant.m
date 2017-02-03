138ef244a15a0656a1d3f36efb75d49286e4b9ef
fa28 <fa28@st-andrews.ac.uk>
2017-02-03 15:20:20 +0000

    changes

--- whit_kal_participant.m
+++ whit_kal_participant.m
@@ -28,15 +28,15 @@ part_PE_Err(1) = 0;
 RO(1)  = 0;
 for i= 2:length (respOri);
     
-    kal_predict(i)=kal_predict(i-1);%kalman
+%     kal_predict(i)=kal_predict(i-1);%kalman
+%     
+%     kal_predict(i)=kal_predict(i-1) + gain*minAngleDiff(stimOri(i),kal_predict(i-1));%kalman
+%     
+%     kal_predict(i) = wrapTo90(kal_predict(i));%kalman wrap function
     
-    kal_predict(i)=kal_predict(i-1) + gain*minAngleDiff(stimOri(i),kal_predict(i-1));%kalman
-    
-    kal_predict(i) = wrapTo90(kal_predict(i));%kalman wrap function
-    
-    naive_Estimate(i)  = stimOri(i-1) + estgain*minAngleDiff(stimOri(i),stimOri(i-1));%naive
-    
-    naive_err(i)= minAngleDiff(naive_Estimate(i), stimOri(i)); %naive
+%     naive_Estimate(i)  = stimOri(i-1) + estgain*minAngleDiff(stimOri(i),stimOri(i-1));%naive
+%     
+%     naive_err(i)= minAngleDiff(naive_Estimate(i), stimOri(i)); %naive
     
     whitney_err(i) = minAngleDiff(respOri(i), stimOri(i)); %whitney error
     
@@ -46,10 +46,10 @@ for i= 2:length (respOri);
     
     partcipant_update(i) = minAngleDiff(respOri(i), respOri(i-1));% how much the partcipnat updates
     
-    kal_PE(i) = minAngleDiff(kal_predict(i-1), stimOri(i));%kalman prediciton error
-    
-    kal_Update(i) =  minAngleDiff(kal_predict(i),kal_predict (i-1));% how much the partcipant updates
-    
+%     kal_PE(i) = minAngleDiff(kal_predict(i-1), stimOri(i));%kalman prediciton error
+%     
+%     kal_Update(i) =  minAngleDiff(kal_predict(i),kal_predict (i-1));% how much the partcipant updates
+%     
     
     
     
@@ -64,7 +64,10 @@ for i= 2:length (respOri);
 
 
 end
-%[R,P]=corrcoef(part_PEErr,RO);
+[a,b]=corrcoef(RO, whitney_err);
+
+[r,p]=corrcoef(part_PE_Err, partcipant_update);
+
 %p=polyfit(sdErr,RO,1);
 var_prox=var(whitney_err); % amount of variance in the error
 var_dist=var(stimOri); %variance in the stimOri
@@ -77,18 +80,18 @@ var_dist=var(stimOri); %variance in the stimOri
 
 
 
-figure(101);
-clf
-set (gca,'fontsize', 24);
-hold on
-plot (respOri,'r', 'Linewidth',3);
+% figure(101);
+% clf
+% set (gca,'fontsize', 24);
 % hold on
-% plot (stimOri,'g', 'Linewidth',2);
-hold on
-plot (kal_predict,'k','Linewidth',3);
-legend ('Participant response','Kalman Prediction');
-xlabel('Trial number');
-ylabel ('Orientation (degs)');
+% plot (respOri,'r', 'Linewidth',3);
+% % hold on
+% % plot (stimOri,'g', 'Linewidth',2);
+% hold on
+% plot (kal_predict,'k','Linewidth',3);
+% legend ('Participant response','Kalman Prediction');
+% xlabel('Trial number');
+% ylabel ('Orientation (degs)');
 
 
 figure(102);
@@ -103,61 +106,61 @@ legend ('Participant error (deg) vs relative orientation(deg)');
 xlabel('Relative orientation of current trial compared to previous trial(deg)');
 ylabel('Participant error on current trial (deg)');
 
-figure (103);
-clf;
-%RO vs PE plot
-set (gca,'fontsize', 24);
-hold on
-scatter (RO, kal_PE,90,'k','filled');
-axis([-70,70,-70,70]);
-legend ('Kalman Prediction error (deg) vs relative orientation(deg)');
-xlabel ('Relative orientation of current trial compared to previous trial(deg)');
-ylabel('Prediction made by Kalman error on current trial (deg)');
+% figure (103);
+% clf;
+% %RO vs PE plot
+% set (gca,'fontsize', 24);
+% hold on
+% scatter (RO, kal_PE,90,'k','filled');
+% axis([-70,70,-70,70]);
+% legend ('Kalman Prediction error (deg) vs relative orientation(deg)');
+% xlabel ('Relative orientation of current trial compared to previous trial(deg)');
+% ylabel('Prediction made by Kalman error on current trial (deg)');
 
 
-figure (104);
-clf;
-%kal estimate vs PE 
-set (gca,'fontsize', 24);
-hold on
-scatter (kal_Update, kal_PE,80,'k','filled');
-axis([-100,100,-100,100]);
-hold on
-legend ('Kalman prediction error (deg) vs Kalman predicition update(deg)');
-xlabel('How much the Kalman updates its next prediction');
-ylabel ('Prediction error on  current prediction');
+% figure (104);
+% clf;
+% %kal estimate vs PE 
+% set (gca,'fontsize', 24);
+% hold on
+% scatter (kal_Update, kal_PE,80,'k','filled');
+% axis([-100,100,-100,100]);
+% hold on
+% legend ('Kalman prediction error (deg) vs Kalman predicition update(deg)');
+% xlabel('How much the Kalman updates its next prediction');
+% ylabel ('Prediction error on  current prediction');
 
 
 
-figure(105);
-clf;
-set(gca,'fontsize', 24);
-hold on
-scatter (RO, naive_Estimate,80,'k','filled');
-axis([-60,60,-60,60]);
-legend ('Naive estimate vs Relative orientation');
-xlabel ('Relative orientation of current trial compared to previous trial(deg)');
-ylabel('Naive estimate error on current trial (deg)');
-
-
+% figure(105);
+% clf;
+% set(gca,'fontsize', 24);
+% hold on
+% scatter (RO, naive_Estimate,80,'k','filled');
+% axis([-60,60,-60,60]);
+% legend ('Naive estimate vs Relative orientation');
+% xlabel ('Relative orientation of current trial compared to previous trial(deg)');
+% ylabel('Naive estimate error on current trial (deg)');
+% 
+% 
 
 figure(106);
 clf;
 set(gca,'fontsize', 24);
 hold on
-scatter (partcipant_update, part_PE_Err,80,'k','filled');
+scatter (part_PE_Err, partcipant_update,80,'k','filled');
 legend ('Partcipant response error (deg) vs partcipant response update(deg)');
 xlabel('How much the participant updates the next response (deg)');
 ylabel ('Participant error on current trial (deg)');
 
-figure(107);
-clf;
-set(gca,'fontsize', 24);
-hold on
-scatter (naive_Estimate, naive_err,80,'k','filled');
-legend ('Naive estimate  error (deg) vs How much the estmate updates(deg)');
-xlabel('How much the naive estimate  updates the next response (deg)');
-ylabel ('Naive estimate error on current trial (deg)');
+% figure(107);
+% clf;
+% set(gca,'fontsize', 24);
+% hold on
+% scatter (naive_Estimate, naive_err,80,'k','filled');
+% legend ('Naive estimate  error (deg) vs How much the estmate updates(deg)');
+% xlabel('How much the naive estimate  updates the next response (deg)');
+% ylabel ('Naive estimate error on current trial (deg)');
 
 
 % figure(108);
