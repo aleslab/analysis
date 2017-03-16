795788c68cd53b12adc1ef64b119947ffb36142c
fa28 <fa28@st-andrews.ac.uk>
2017-03-16 13:41:59 +0000

    development of n back analysis code

--- whit_kal_project_n_back.m
+++ whit_kal_project_n_back.m
@@ -39,18 +39,18 @@ for iParticipant = 1 : ptbCorgiData.nParticipants,
             
              err(i) = minAngleDiff(respOri(i), stimOri(i));%whitney
              RO(i)=  minAngleDiff (stimOri(i-1),stimOri (i));%whitney
-             RO_n_back1(i)= minAngleDiff (stimOri(i-2),stimOri (i));
-             RO_n_back2(i) = minAngleDiff (stimOri(i-3), stimOri(i));
-             RO_n_back3(i)= minAngleDiff (stimOri(i-4), stimOri(i));
+             RO_n_back_2(i)= minAngleDiff (stimOri(i-2),stimOri (i));
+             RO_n_back_3(i) = minAngleDiff (stimOri(i-3), stimOri(i));
+             RO_n_back_4(i)= minAngleDiff (stimOri(i-4), stimOri(i));
              
         end
         
         
-        err = err(2:end);
-        RO  = RO(2:end);
-        RO_n_back1(2:end);
-        RO_n_back2(2:end);
-        RO_n_back3(2:end);
+        err = err(5:end);
+        RO  = RO(5:end);
+        RO_n_back_2=RO_n_back_2(5:end);
+        RO_n_back_3=RO_n_back_3(5:end);
+        RO_n_back_4=RO_n_back_4(5:end);
              
              
         figure(100+iParticipant);        
@@ -64,16 +64,16 @@ for iParticipant = 1 : ptbCorgiData.nParticipants,
         
         %Calculate the correlation coefficient
         [r p ]= corrcoef(RO, err);
-        whitneySD(iParticipant,iCond).r = r(1,2);
-        whitneySD(iParticipant,iCond).p = p(1,2);
+        n_back_1_corr(iParticipant,iCond).r = r(1,2);
+        n_back_1_corr(iParticipant,iCond).p = p(1,2);
         %calyculate regression slopes
         myModel = cat(1,RO,ones(size(RO)))';
         myY     = err';
         [b bint] = regress(myY, myModel);
-        whitneyFit(iParticipant,iCond).b = b;
-        whitneyFit(iParticipant,iCond).bint = bint;
-        whitneySlope(iParticipant,iCond) = b(1);
-        whitneySlopeInt(iParticipant,iCond,:) = bint(1,:);
+        n_back_1_Fit(iParticipant,iCond).b = b;
+        n_back_1_Fit(iParticipant,iCond).bint = bint;
+        n_back_1_Slope(iParticipant,iCond) = b(1);
+        n_back_1_SlopeInt(iParticipant,iCond,:) = bint(1,:);
         
         
         %Now get 
@@ -81,8 +81,8 @@ for iParticipant = 1 : ptbCorgiData.nParticipants,
         axis square
         hold on
         title({thisLabel; ...
-            [' r: ' num2str(whitneySD(iParticipant,iCond).r) ...
-            ' p: ' num2str(whitneySD(iParticipant,iCond).p) ]});
+            [' r: ' num2str(n_back_1_corr(iParticipant,iCond).r) ...
+            ' p: ' num2str(n_back_1_corr(iParticipant,iCond).p) ]});
         
         if iCond ==1
         xlabel('Relative orientation of current trial compared to previous trial(deg)');
@@ -95,28 +95,117 @@ for iParticipant = 1 : ptbCorgiData.nParticipants,
         set(gcf,'position',figurePosition);
         
         
-        figure(100+iParticipant);        
+        figure(200+iParticipant);        
         %put all conditions in 1 plot.
         subplot(1,ptbCorgiData.nConditions,iCond)
         %whitney plot
         set(gca,'fontsize', 16);
         hold on
-        scatter (RO_n_back1, err, 40,'k','filled');
+        scatter ( RO_n_back_2, err,40,'k','filled');
         %add a regresion line
         
         %Calculate the correlation coefficient
-        [r p ]= corrcoef(RO_n_back1, err);
-        whitney_n_back1(iParticipant,iCond).r = r(1,2);
-        whitney_n_back1(iParticipant,iCond).p = p(1,2);
+        [r p ]= corrcoef(RO_n_back_2, err);
+        n_back_2_corr(iParticipant,iCond).r = r(1,2);
+        n_back_2_corr(iParticipant,iCond).p = p(1,2);
         %calyculate regression slopes
-        myModel = cat(1,RO_n_back1,ones(size(RO)))';
+        myModel = cat(1,RO_n_back_2,ones(size(RO_n_back_2)))';
         myY     = err';
         [b bint] = regress(myY, myModel);
-        whitney_n_back1_Fit(iParticipant,iCond).b = b;
-        whitney_n_back1_Fit(iParticipant,iCond).bint = bint;
-        whitney_n_back1_Slope(iParticipant,iCond) = b(1);
-        whitney_nback_1_SlopeInt(iParticipant,iCond,:) = bint(1,:);
+        n_back_2_Fit(iParticipant,iCond).b = b;
+        n_back_2_Fit(iParticipant,iCond).bint = bint;
+        n_back_2_Slope(iParticipant,iCond) = b(1);
+        n_back_2_SlopeInt(iParticipant,iCond,:) = bint(1,:);
         
+        
+        %Now get 
+        axis([-90,90,-90,90]);
+        axis square
+        hold on
+        title({thisLabel; ...
+            [' r: ' num2str(n_back_2_corr(iParticipant,iCond).r) ...
+            ' p: ' num2str(n_back_2_corr(iParticipant,iCond).p) ]});
+        
+        if iCond ==1
+        xlabel('Relative orientation of current trial compared to previous trial(deg)');
+        ylabel('Error on current trial (deg)');
+        end
+        thisFilename = [ptbCorgiData.paradigmName ...
+            '_' thisParticipantId '_fischerSD'];
+
+        set(gcf,'FileName',thisFilename)
+        set(gcf,'position',figurePosition);
+        
+        
+        
+       
+        figure(300+iParticipant);        
+        %put all conditions in 1 plot.
+        subplot(1,ptbCorgiData.nConditions,iCond)
+        %whitney plot
+        set(gca,'fontsize', 16);
+        hold on
+        scatter ( RO_n_back_3, err,40,'k','filled');
+        %add a regresion line
+        
+        %Calculate the correlation coefficient
+        [r p ]= corrcoef(RO_n_back_3, err);
+        n_back_3_corr(iParticipant,iCond).r = r(1,2);
+        n_back_3_corr(iParticipant,iCond).p = p(1,2);
+        %calyculate regression slopes
+        myModel = cat(1,RO_n_back_3,ones(size(RO_n_back_3)))';
+        myY     = err';
+        [b bint] = regress(myY, myModel);
+        n_back_3_Fit(iParticipant,iCond).b = b;
+        n_back_3_Fit(iParticipant,iCond).bint = bint;
+        n_back_3_Slope(iParticipant,iCond) = b(1);
+        n_back_3_SlopeInt(iParticipant,iCond,:) = bint(1,:);
+        
+        
+        %Now get 
+        axis([-90,90,-90,90]);
+        axis square
+        hold on
+        title({thisLabel; ...
+            [' r: ' num2str(n_back_3_corr(iParticipant,iCond).r) ...
+            ' p: ' num2str(n_back_3_corr(iParticipant,iCond).p) ]});
+        
+        if iCond ==1
+        xlabel('Relative orientation of current trial compared to previous trial(deg)');
+        ylabel('Error on current trial (deg)');
+        end
+        thisFilename = [ptbCorgiData.paradigmName ...
+            '_' thisParticipantId '_fischerSD'];
+
+        set(gcf,'FileName',thisFilename)
+        set(gcf,'position',figurePosition);
+        
+        
+        
+       %Linear Regression to fit n-back 
+       %Or equation: R = a*S(i) + b*S(i-1)...
+       % R = myModel*b + bint
+       myModel = cat(1,stimOri(5:end),stimOri(4:end-1), stimOri(3:end-2),...
+           stimOri(2:end-3),stimOri(1:end-4),ones(size(RO_n_back_3)))';
+       myY     = respOri(5:end)';
+       
+        [b bint] = regress(myY, myModel);
+        n_back_full_fit(iParticipant,iCond).b = b;
+        n_back_full_fit(iParticipant,iCond).bint = bint;
+        n_back_full_Slope(iParticipant,iCond,:) = b;
+        n_back_full_SlopeInt(iParticipant,iCond,:) = bint(1,:);
+        
+        %Plot slopes
+        figure(1000+iParticipant);        
+        %put all conditions in 1 plot.
+        subplot(1,ptbCorgiData.nConditions,iCond)
+        errorbar(0:4,b(1:5),bint(1:5,1)-b(1:5),bint(1:5,2)-b(1:5))
+       
+        %Analysis looking at stimulus ONLY
+        myModel = cat(1,stimOri(4:end-1), stimOri(3:end-2),...
+        stimOri(2:end-3),stimOri(1:end-4),ones(size(RO_n_back_3)))';
+        myY     = stimOri(5:end)';
+       [b bint] = regress(myY, myModel)
         
         
     end
