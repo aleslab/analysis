cc7d65a8536e4b9715f84aa8420f3a3ad0f35dfe
Justin Ales <justin.ales@gmail.com>
2017-03-20 11:46:12 +0000

    Added functions to support circular correlations.

--- responseSimulation/simulateStimulusAverage.m
+++ responseSimulation/simulateStimulusAverage.m
@@ -1,5 +1,18 @@
-function [ response ] = simulateStimulusAverage( stimulus, weights )
+function [ response ] = simStimAve( stimulus, weights, proximalVariance )
 
+%Double the angle, because we have a symmetric stim
+stimulus = 2*stimulus;
+stimSin = sind(stimulus);
+stimCos = cosd(stimulus);
 
+nWeights = length(weights);
+nStim    = length(stimulus);
+
+for iTrial = nWeights:nStim,
+    
+respSin(iTrial) = weights.*simSin((iTrial-nWeight+1):iTrial);
+respCos(iTrial) = weights.*simCos((iTrial-nWeight+1):iTrial);
 end
 
+response = atan2(respSin,respCos);
+respons = response/2;
