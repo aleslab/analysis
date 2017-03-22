7264b1851af069779176f06c84b624adad8be0b0
fa28 <fa28@st-andrews.ac.uk>
2017-03-22 15:14:15 +0000

    attempting to build sim function

--- responseSimulation/simulateStimulusAverage.m
+++ responseSimulation/simulateStimulusAverage.m
@@ -2,10 +2,14 @@ function [ response ] = simulateStimulusAverage( stimulus, weights )
 
 
 
-for i= 2:length (respOri);
+for i= 2:length (stimulus);
     
-    resposne(i) = stimuli(i)*weights(i)+stimulus(i-1)*weights
+    resposne(i) = stimulus(i)*weights(i)+stimulus(i-1)*weights(i-2)+stimulus*weights(i-3);
+    
+end
 
+
+response = resposne (1:end);
 
 end
 
