e3116423507b89c592bb6de5580f725f78b069f6
fraser <fa28@st-andrews.ac.uk>
2017-03-28 13:54:49 +0100

    stim average changes

--- responseSimulation/simulateStimulusAverage.m
+++ responseSimulation/simulateStimulusAverage.m
@@ -1,15 +1,18 @@
 function [ response ] = simulateStimulusAverage( stimulus, weights )
 
 
-respOri=wrapTo90(respOri);
-stimOri=wrapTo90(stimOri);
+% respOri=wrapTo90(respOri);
+stimulus=wrapTo90(stimulus);
 
-for i= 2:length (stimulus);
+nweights = length(weights);
+
+for i= nweights:length (stimulus);
     
     response(i) = stimulus(i)*weights(i)+stimulus(i-1)*weights(i-1)+stimulus(i-2)*weights(i-2);
     
 end
 
+minAngleDiff(response,90) <=tol;
 
 response  = response (3:end);
 weights = weights (3:end);
