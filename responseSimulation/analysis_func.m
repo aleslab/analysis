356f83c53feae6e41107a96c71766442b253e0f5
fraser <fa28@st-andrews.ac.uk>
2017-04-11 12:11:13 +0100

    name change of analysis function?

--- responseSimulation/analysis_func.m
+++ responseSimulation/analysis_func.m
@@ -1,16 +1,16 @@
 
 
-function [ r, p, b, bint ] = analysis_func ( input_1, input_2)
+function [ b, bint, r, p ] = circularSlope90d( X, Y)
 
 
 %[ b, bint, r, p] = circularSlope90d( Y,X )
     
         %Calculate the correlation coefficient
-        [b, bint, r,  p ]= circularSlope90d(input_1, input_2);
+        [r,  p ]= circularSlope90d(X, Y);
          %calyculate regression slopes
-        myModel = cat(1,input_1,ones(size(input_1)))';
-        myY     = input_2';
-        [b bint] = regress(myY, myModel);
+        myModel = cat(1,X,ones(size(X)))';
+        myY     = Y';
+        [b, bint] = regress(myY, myModel);
         
 end
 
