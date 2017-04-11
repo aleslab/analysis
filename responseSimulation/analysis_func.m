ae3a2cb088699d58b0083c654cdb7c282aac74d2
fraser <fa28@st-andrews.ac.uk>
2017-04-11 11:54:52 +0100

    added circular slope90d to function

--- responseSimulation/analysis_func.m
+++ responseSimulation/analysis_func.m
@@ -3,10 +3,10 @@
 function [ r, p, b, bint ] = analysis_func ( input_1, input_2)
 
 
-
+%[ b, bint, r, p] = circularSlope90d( Y,X )
     
         %Calculate the correlation coefficient
-        [r p ]= corrcoef(input_1, input_2);
+        [b, bint, r,  p ]= circularSlope90d(input_1, input_2);
          %calyculate regression slopes
         myModel = cat(1,input_1,ones(size(input_1)))';
         myY     = input_2';
