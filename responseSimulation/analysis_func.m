fb36e405d378ca76a828bedcbf2d7a0a57d4d701
fraser <fa28@st-andrews.ac.uk>
2017-04-11 15:58:54 +0100

    changed back

--- responseSimulation/analysis_func.m
+++ responseSimulation/analysis_func.m
@@ -1,15 +1,15 @@
 
 
-%function [ b, bint, r, p ] = analysis_func( input_1, input_2)
+function [ b, bint, r, p ] = analysis_func( X, Y)
 
 
-    function [ b, bint, r, p] = circularSlope90d( input_1,input_2 )
+    
     
         %Calculate the correlation coefficient
-        [r,  p ]= corr(input_1, input_2);
+        [r,  p ]= circularSlope90d(X, Y);
          %calyculate regression slopes
-        myModel = cat(1,input_1,ones(size(input_1)))';
-        myY     = input_2';
+        myModel = cat(1,X,ones(size(X)))';
+        myY     = Y';
         [b, bint] = regress(myY, myModel);
         
 end
