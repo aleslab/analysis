1bd181f9794b9ad491b48f79e9e135d6096b4983
fraser <fa28@st-andrews.ac.uk>
2017-04-11 12:19:45 +0100

    change regression to circ90d

--- responseSimulation/analysis_func.m
+++ responseSimulation/analysis_func.m
@@ -1,16 +1,16 @@
 
 
-function [ b, bint, r, p ] = circularSlope90d( X, Y)
+function [ b, bint, r, p ] = analysis_func( X, Y)
 
 
 %[ b, bint, r, p] = circularSlope90d( Y,X )
     
         %Calculate the correlation coefficient
-        [r,  p ]= circ_corrccd90(X, Y);
+        [r,  p ]= circularSlope90d(X, Y);
          %calyculate regression slopes
         myModel = cat(1,X,ones(size(X)))';
         myY     = Y';
-        [b, bint] = regress(myY, myModel);
+        [b, bint] = circularSlope90d(myY, myModel);
         
 end
 
