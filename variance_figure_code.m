1e6c8ab9ce2664b8403e8ef6e2c81aacc7e1ac32
fraser <fa28@st-andrews.ac.uk>
2017-04-21 22:40:50 +0100

    plotting

--- variance_figure_code.m
+++ variance_figure_code.m
@@ -1,5 +1,5 @@
-v= [481 122 107]';% put variance figures in one condition at a time i.e all con1's then all con2's
+v= [ 564 281 136]';% put variance figures in one condition at a time i.e all con1's then all con2's
 
-bar1=bar(v, 'grouped');
-ylabel('condition'); % y-axis label
-ylabel('proximal variance (var in error'); % y-axis label
+bar1=bar(v, 'grouped', 'k');
+ylabel('5% 10% 20%'); % y-axis label
+ylabel('Proximal variance'); % y-axis label
