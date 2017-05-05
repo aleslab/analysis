347898dc4e6d3f50a2cbc42d2daa93474fc52856
fraser <fa28@st-andrews.ac.uk>
2017-05-05 09:56:36 +0100

    new sim

--- average_sim.m
+++ average_sim.m
@@ -5,12 +5,15 @@ fileToLoad = uigetfile; load(fileToLoad);
 [sortedData] = organizeData(sessionInfo,experimentData);
 
 
-iCond =3; %When you have only 1 condition
+iCond =1; %When you have only 1 condition
 respOri = [sortedData(iCond).trialData(:).respOri];
 stimOri = [sortedData(iCond).trialData(:).stimOri];
 
 
-[ weights, responseHat, residual] = fitCircularStimulusAverage( stimOri,respOri,6 );
+[ weights, responseHat, residual] = fitCircularStimulusAverage( stimOri,respOri,5 );
 
-
+figure(101);
+clf
+set (gca,'fontsize', 24);
+bar(weights)
 
