8a16d399d9e347cbe31ea09a4c2632799495ca75
fraser <fa28@st-andrews.ac.uk>
2017-08-01 17:15:57 +0100

    plot develop

--- Bar_graph_build_FA.m
+++ Bar_graph_build_FA.m
@@ -1,17 +1,19 @@
-Proximal_variance = [355.20; 247.90; 141];
-error = [75; 85; 28];
-bar(Proximal_variance,'FaceColor',[0.4,0.4,0.4],'EdgeColor',[0 .01 .01],'LineWidth',0.5);
+N_backs = [1.08; 0.01; 0.01; 0.00; 0.00; 0.00; 0.00; 0.00];
+error = [0.10; 0.09; 0.11; 0.07; 0.12; 0.05; 0.11; 0.09 ];
+%bar(N_backs,'FaceColor',[0.3,0.3,0.3],'EdgeColor',[0 .01 .01],'LineWidth',0.5);
 %set('BarWidth',0.6); 
-ylabel('Mean proximal variance');
+boxplot(N_backs,'LineWidth',0.5);
+ylabel('weight on current trial');
 xlabel('Contrast condition'); 
-Labels = {'5%', '20%', '10%'};
-set(gca, 'XTick', 1:4, 'XTickLabel', Labels, 'fontsize', 22);
+Labels = {'n-1',  'n-2', 'n-3', 'n-4', 'n-5', 'n-6','n-7', 'n-8'};
+set(gca, 'XTick', 1:8, 'XTickLabel', Labels, 'fontsize', 22);
+legend ('Fitted weights 5% contrast experiment one');
 hold on
-numgroups=size(Proximal_variance,1);
-numbars=size(Proximal_variance,2);
-groupwidth=min(0.4, numbars/(numbars+1.1));
+numgroups=size(N_backs,1);
+numbars=size(N_backs,2);
+groupwidth=min(0.2, numbars/(numbars+1.1));
 for i = 1:numbars
       % Based on barweb.m by Bolu Ajiboye from MATLAB File Exchange
       x = (1:numgroups) - groupwidth/2 + (2*i-1) * groupwidth / (2*numbars);  % Aligning error bar with individual bar
-      errorbar(x, Proximal_variance(:,i), error(:,i), 'k', 'linestyle', 'none');
+      errorbar(x, N_backs(:,i), error(:,i), 'k', 'linestyle', 'none');
 end
