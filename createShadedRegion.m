84cf354da59fb26d370e8b8c932b6264546058ea
Justin Ales <justin.ales@gmail.com>
2019-03-13 10:45:08 +0000

    Added ability for create shaded region to draw multiple plots at once.  Added some error checking and edge case handling of when yLo==yHi

--- createShadedRegion.m
+++ createShadedRegion.m
@@ -22,15 +22,28 @@ function [ hLine, hPatch ] = createShadedRegion( x,y,yLo,yHi,varargin )
 
 %TODO: Error checking!
 
-%These points will be used to create the patch that we will make
-%transparent
-px = [x(:); flipud(x(:))];    % use px=[x1;x2(end:-1:1)]; if row vecs
-py = [ yLo(:) ; flipud(yHi(:)) ];    % use ...
+if ~isequal(size(x),size(y),size(yLo),size(yHi)) 
+    error('Input to function not correct,  all matrices must have have equal sizes')
+end
+
 
 %Plot the line. 
 hLine = plot(x,y,varargin{:});
 
-hPatch=patch(px,py,hLine.Color,'linestyle','none','facealpha',.25);
+%Need a kludge factor to stop problems when area of shaded region is 0. 
+kludge = 1e-6*max(y(:));
+for iPatch = 1:length(hLine)
+    
+    thisX = x(:,iPatch);
+    thisYLo = yLo(:,iPatch);
+    thisYHi = yHi(:,iPatch);
+    %These points will be used to create the patch that we will make
+    %transparent
+    px = [thisX(:); flipud(thisX(:))];    % use px=[x1;x2(end:-1:1)]; if row vecs
+    py = [ thisYLo(:)-kludge ; flipud(thisYHi(:))+kludge ];    % use ...
+
+    hPatch=patch(px,py,hLine(iPatch).Color,'linestyle','none','facealpha',.25);
+end
 
 end
 
